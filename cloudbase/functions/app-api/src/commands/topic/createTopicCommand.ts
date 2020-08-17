e34d8aa2c5be15a730ef8c99b69b94e6fdffddf3
Seven Du <shiweidu@outlook.com>
2020-08-17 11:15:08 +0800

    chore(WIP): chreated topic return topic id

--- cloudbase/functions/app-api/src/commands/topic/createTopicCommand.ts
+++ cloudbase/functions/app-api/src/commands/topic/createTopicCommand.ts
@@ -67,7 +67,7 @@ async function handler(app: App) {
 
     const result = await database.collection('topics').add({ corator_id: creatorUserId, name, description, title, cover, joinType, postType  });
 
-    return !!result.id;
+    return result.id;
 }
 
 export function createTopicCommand(app: App) {
