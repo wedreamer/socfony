58992c95cb2e7ed6fd826f14febd09d09c81f580
Seven Du <shiweidu@outlook.com>
2020-08-26 14:18:05 +0800

    Revert "chore: change version"
    This reverts commit 7a1718409435d7311ecfd89559272f4bef234384.

--- scripts/change-flutter-app-version.js
+++ scripts/change-flutter-app-version.js
@@ -2,14 +2,10 @@ const path = require('path');
 const fs = require('fs');
 
 const pubspecPath = path.resolve(__dirname, '..', 'pubspec.yaml');
+const { version } = require('../package.json');
 const contents = fs.readFileSync(pubspecPath).toString();
-const patten = /^(\d{1,2}.\d{1,2}).(\d{1,2})$/;
-const version = process.argv[2];
-
-const [_, v1, v2] = patten.exec(version);
-
 
 fs.writeFileSync(pubspecPath, contents.replace(
     /version\:(.*)\#\@cli,Date:(\d+)/,
-    'version: ' + v1 + '+' + v2 + ' #@cli,Date:' + Date.now(),
+    'version: ' + version.replace('-', '+') + ' #@cli,Date:' + Date.now(),
 ));
