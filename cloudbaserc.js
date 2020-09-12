b2bcb3af3b4e64fc2929983f184759a2e48bf5a8
Seven Du <shiweidu@outlook.com>
2020-09-12 14:26:59 +0800

    chore: *

--- cloudbaserc.js
+++ cloudbaserc.js
@@ -14,7 +14,6 @@ const functions = {
       // 忽略 TypeScript 源文件，不需要安装时候编译
       "src",
       "src/**/*",
-
       // 忽略 npm 锁文件，避免系统不一致所导致的云函数安装失败
       'package-lock.json',
     ],
@@ -31,21 +30,6 @@ const cli = {
 // @cloudbase/framework 配置信息
 const framework = {
   name: 'Fans',
-  // // 暂不启用，手动添加服务地址
-  // plugins: {
-  //   server: {
-  //     use: '@cloudbase/framework-plugin-node',
-  //     inputs: {
-  //       entry: 'index.js',
-  //       path: '/service',
-  //       name: functions.server.name,
-  //       projectPath: 'packages/server',
-  //       buildCommand: "npm run lerna -- run --scope server build",
-  //       platform: 'function',
-  //       functionOptions: functions.server,
-  //     },
-  //   },
-  // },
 };
 
 module.exports = { ...cli, framework };
