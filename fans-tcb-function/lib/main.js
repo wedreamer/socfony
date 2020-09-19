5a1bdadba4f554500872a0283c3744f690e29e94
Seven Du <shiweidu@outlook.com>
2020-09-19 17:35:04 +0800

    chore: 移除用不到的包

--- fans-tcb-function/lib/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.plugin = void 0;
-var plugin_1 = require("./plugin");
-Object.defineProperty(exports, "plugin", { enumerable: true, get: function () { return plugin_1.plugin; } });
-//# sourceMappingURL=main.js.map
--- fans-tcb-function/src/inputs.d.ts
+++
@@ -1,9 +0,0 @@
-export interface CAccessOption {
-    function: string;
-    path: string;
-    auth?: boolean;
-}
-
-export interface CPlugincInputs {
-    access?: CAccessOption[];
-}
