2fac0120476300b9e4c344c312fff1ffeade38a6
Seven Du <shiweidu@outlook.com>
2020-09-21 15:52:19 +0800

    chore(WIP): 完善已有的动态集合

--- packages/database/collections/index.js
+++ packages/database/collections/index.js
@@ -1,9 +1,16 @@
 module.exports = [
     // 动态
     require('./moments.json'),
+    require('./moment-comments.json'),
     require('./moment-liked-histories.json'),
+    require('./moment-vote-user-selected.json'),
 
     // 用户交友卡
     require('./user-card-categories.json'),
     require('./user-cards.json'),
+
+    // 话题相关
+    require('./topic-categories.json'),
+    require('./topics.json'),
+    require('./topic-user.json'),
 ];
