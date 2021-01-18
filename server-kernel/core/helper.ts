ec2981b4ce71f1ea39101b9d61048440263d8f70
Seven Du <shiweidu@outlook.com>
2021-01-18 16:03:32 +0800

    WIP

--- server-kernel/core/helper.ts
+++ server-kernel/core/helper.ts
@@ -1,11 +1,11 @@
-import { customAlphabet, nanoid } from 'nanoid';
+import { NanoID } from '~deps';
 
 /**
  * create [a-Z0-9-_] Nano ID.
  * @param length need create size.
  */
 export function nanoIdGenerator(length: number): string {
-  return nanoid(length);
+  return NanoID.nanoid(length);
 }
 
 /**
@@ -13,7 +13,7 @@ export function nanoIdGenerator(length: number): string {
  * @param length need create size.
  */
 export function numberNanoIdGenerator(length: number): string {
-  return customAlphabet('1234567890', length)();
+  return NanoID.customAlphabet('1234567890', length)();
 }
 
 /**
@@ -21,7 +21,10 @@ export function numberNanoIdGenerator(length: number): string {
  * @param length need create size.
  */
 export function alphabetNanoIdGenerator(length: number): string {
-  return customAlphabet('qwertyuiopasdfghjklzxcvbnm'.toUpperCase(), length)();
+  return NanoID.customAlphabet(
+    'qwertyuiopasdfghjklzxcvbnm'.toUpperCase(),
+    length,
+  )();
 }
 
 /**
@@ -29,7 +32,7 @@ export function alphabetNanoIdGenerator(length: number): string {
  * @param length need create size.
  */
 export function readabilityNanoIdGenerator(length: number): string {
-  return customAlphabet(
+  return NanoID.customAlphabet(
     '23456789qwertyupasdfghjklzxcvbnm'.toUpperCase(),
     length,
   )();
--- ~deps/index.ts
+++ ~deps/index.ts
@@ -1,5 +1,5 @@
 export * as Kernel from '../server-kernel';
 export * as NestJS from './nestjs';
 export * as TencentCloud from './tencent-cloud';
-
 export { default as dayjs } from 'dayjs';
+export * as NanoID from 'nanoid';
