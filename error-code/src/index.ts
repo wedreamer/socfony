32143a149d8be9c5e60a6dc00629668ce5112db2
Seven Du <shiweidu@outlook.com>
2021-01-19 20:23:44 +0800

    chore(ErrorCode): add error code module

--- error-code/src/index.ts
+++ error-code/src/index.ts
@@ -1,14 +1,14 @@
-// 用户未登录
+// User unauthorized
 export const UNAUTHORIZED = 'UNAUTHORIZED';
 // Authorization Token Not Found.
 export const AUTHORIZATION_TOKEN_NOT_FOUND = 'AUTHORIZATION_TOKEN_NOT_FOUND';
 
-// 用户没有设置密码
+// User not set password
 export const USER_NOT_SET_PASSWORD = 'USER_NOT_SET_PASSWORD';
-// 用户密码验证错误
+// user password no compare
 export const USER_PASSWORD_NOT_COMPARE = 'USER_PASSWORD_NOT_COMPARE';
-// 用户不存在
+// user not found
 export const USER_NOT_FOUND = 'USER_NOT_FOUND';
 
-// 安全代码验证失败
+// security code validate error
 export const SECURITY_VALIDATE_ERROR = 'SECURITY_VALIDATE_ERROR';
